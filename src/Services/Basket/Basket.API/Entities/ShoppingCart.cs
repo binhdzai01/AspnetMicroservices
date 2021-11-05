af3128c5ff9ece209fd8fd3898640ea817721d31
Hoang Duc Binh <hoangducbinh123123@gmail.com>
2021-11-06 03:04:27 +0700

    done Basket API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(String UserName)
+        {
+            this.UserName = UserName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach(var item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+    }
+}
