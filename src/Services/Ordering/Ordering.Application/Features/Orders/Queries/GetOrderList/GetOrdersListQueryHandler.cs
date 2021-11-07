5fca4e91a9b8ac31507e9c8b43c49e1ad95d5bfe
Hoang Duc Binh <hoangducbinh123123@gmail.com>
2021-11-07 23:21:17 +0700

    git done Commands and Queries

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery,List<OrdersVm>>
+    {
+
+        private readonly IOrderRepository _repository;
+
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository repository , IMapper mapper)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request,
+           CancellationToken cancellationToken)
+        {
+            var orderList = await _repository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedby { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
