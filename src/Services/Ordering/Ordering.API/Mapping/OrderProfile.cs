544e82cbc5b9c0b2385c083a83a94c7e9bc6521f
Binh <hoangducbinh123123@gmail.com>
2021-11-09 23:55:17 +0700

    done with rabbit MQ

---
+++ src/Services/Ordering/Ordering.API/Mapping/OrderProfile.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Messages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using AutoMapper;
+namespace Ordering.API.Mapping
+{
+    public class OrderProfile:Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
